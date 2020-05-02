07f4ebe4b1972841978f693be671021d8ca1d112
Luis Eduardo <luis.diaz.car@gmail.com>
2020-05-02 15:55:49 -0500

    implement show candidates

--- talent-front/src/components/ShowCandidates.js
+++ talent-front/src/components/ShowCandidates.js
@@ -1,38 +1,96 @@
-import React from "react";
-import Table from "react-bootstrap/Table";
-class ShowCandidates extends React.Component {
+import React, { Component } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+
+class ShowCandidates extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columnDefs: [
+        {
+          headerName: "Name",
+          field: "name",
+          minWidth: 180,
+          headerCheckboxSelection: true,
+          headerCheckboxSelectionFilteredOnly: true,
+          checkboxSelection: true,
+        },
+        {
+          headerName: "Email",
+          field: "email",
+          minWidth: 200,
+        },
+        {
+          headerName: "Street",
+          field: "address.street",
+        },
+
+        {
+          headerName: "Suite",
+          field: "address.suite",
+        },
+
+        {
+          headerName: "City",
+          field: "address.city",
+        },
+        {
+          headerName: "Zipcode",
+          field: "address.zipcode",
+        },
+        {
+          headerName: "Lat",
+          field: "address.geo.lat",
+        },
+        {
+          headerName: "Lng",
+          field: "address.geo.lng",
+        },
+      ],
+      defaultColDef: {
+        flex: 1,
+        minWidth: 100,
+        resizable: true,
+      },
+      rowSelection: "multiple",
+      rowData: [],
+      selectedCandidates: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ rowData: this.props.rowData });
+    console.log(this.props.rowData);
+  }
+  onGridReady = (params) => {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+  };
+
+  onQuickFilterChanged = () => {
+    this.gridApi.setQuickFilter(document.getElementById("quickFilter").value);
+  };
+
+  onSelectionChanged = (params) => {
+    const candidates = params.api.getSelectedRows();
+    this.setState({ selectedCandidates: candidates });
+  };
+
   render() {
     return (
       <div className="container">
-        <Table striped bordered hover size="sm">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td colSpan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
+        <div className="ag-theme-alpine" style={{ height: "350px" }}>
+          <AgGridReact
+            columnDefs={this.state.columnDefs}
+            defaultColDef={this.state.defaultColDef}
+            suppressRowClickSelection={true}
+            rowSelection={this.state.rowSelection}
+            onGridReady={this.onGridReady}
+            onSelectionChanged={this.onSelectionChanged}
+            rowData={this.state.rowData}
+          />
+        </div>
       </div>
     );
   }
